4a75413888deab83a02209b788d7e84b2be4ff49
Rion <rioneffendy0001repo@gmail.com>
2024-08-02 23:14:10 +0700

    Create say_hello_world.py

---
+++ say_hello_world.py
@@ -0,0 +1,15 @@
+# HackerRank - Python
+# https://www.hackerrank.com/challenges/py-hello-world/problem?isFullScreen=true
+
+# -- Cara 1 ---
+print("Hello, World!")
+
+# -- Cara 2 ---
+my_string = "Hello, World!"
+print(my_string)
+
+# -- Cara 3 ---
+# Menggunakan module sys
+import sys
+my_string = "Hello, World!"
+sys.stdout.write(my_string)
