ca0e0fdf24d6a43d2948f25fb698c2fec932f228
Rion <rioneffendy0001repo@gmail.com>
2024-08-03 23:34:25 +0700

    Create arithmetic_operators.py

---
+++ arithmetic_operators.py
@@ -0,0 +1,20 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a + b)
+    print(a - b)
+    print(a * b)
+
+# # Bisa juga hasilnya disimpan ke variable terlebih dahulu
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+
+#     s = a + b
+#     d = a - b
+#     p = a * b
+    
+#     print(s)
+#     print(d)
+#     print(p)
