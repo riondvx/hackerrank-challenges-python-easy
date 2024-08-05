ecef09529c61055f11d7c3b3c986d21fca63fa09
Rion <rioneffendy0001repo@gmail.com>
2024-08-05 17:14:02 +0700

    Create python_division.py

---
+++ python_division.py
@@ -0,0 +1,35 @@
+# HackerRank
+# https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    d1 = a // b # integer division
+    d2 = a / b
+    
+    print(d1)
+    print(d2)
+
+# # Cara Ke 2
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+    
+#     d1 = int(a / b)
+#     d2 = a / b
+    
+#     print(d1)
+#     print(d2)
+
+# # Cara Ke 3
+# import math
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+    
+#     d1 = math.floor(a / b)
+#     d2 = a / b
+    
+#     print(d1)
+#     print(d2)
