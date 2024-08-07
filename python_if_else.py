e2d6a1cce8b63011186a1a4f737d2a32626b5750
Rion <rioneffendy0001repo@gmail.com>
2024-08-07 16:46:11 +0700

    Create python_if_else.py

---
+++ python_if_else.py
@@ -0,0 +1,15 @@
+# HackerRank
+# https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    if n % 2 == 1:
+        print("Weird")
+    else:
+        if 2 <= n <= 5:
+            print("Not Weird")
+        elif 6 <= n <= 20:
+            print("Weird")
+        elif n > 20:
+            print("Not Weird")
