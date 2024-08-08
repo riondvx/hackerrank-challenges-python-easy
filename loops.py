a6aa7e442d5c7ef3bdda20266c39d58c92d783fe
Rion <rioneffendy0001repo@gmail.com>
2024-08-08 21:24:09 +0700

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,19 @@
+# HackerRank
+# https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    i = 0
+    
+    while i < n:
+        hasil = i ** 2
+        print(hasil)
+        i += 1
+
+# # Cara Ke 2
+# if __name__ == '__main__':
+#     n = int(input())
+    
+#     for i in range(0, n): # 0 - (n -1), 0,1,2
+#         hasil = i ** 2
+#         print(hasil)
